0b817c0fad965daf369900b1757fbdc62f1a6579
matr1xc0in <34523785+matr1xc0in@users.noreply.github.com>
2018-08-11 00:53:18 -0700

    Revert bc-ipfs-example tweak, added disableHostCheck for bc-ipfs-example webpack

--- bc-ipfs-example/server.js
+++ bc-ipfs-example/server.js
@@ -1,14 +1,13 @@
 'use strict'
-'use esversion:6'
-let webpack = require('webpack');
-let WebpackDevServer = require('webpack-dev-server');
-let config = require('./webpack.config');
+let webpack = require('webpack')
+let WebpackDevServer = require('webpack-dev-server')
+let config = require('./webpack.config')
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   historyApiFallback: true
 }).listen(3000, '0.0.0.0', function (err) {
-  if (err) throw new Error(err);
+  if (err) throw new Error(err)
   console.log('Listening at 0.0.0.0:3000')
-});
+})

231c2f095d4f6daadf5988f1fad4388df8dd545d
alee-blocksync <alee@blcksync.com>
2018-08-18 07:26:15 -0700

    Added react-bootstrap and simple button for testing

--- bc-ipfs/src/App.js
+++ bc-ipfs/src/App.js
@@ -1,21 +1,57 @@
+/*jshint esversion: 6*/
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Grid, Button } from 'react-bootstrap';
+
+import ipfs from './ipfs';
 
 class App extends Component {
+
+  captureFile (event) {
+    event.stopPropagation();
+    event.preventDefault();
+    const file = event.target.files[0];
+    let reader = new window.FileReader();
+    reader.onloadend = () => this.saveToIpfs(reader);
+    reader.readAsArrayBuffer(file);
+  }
+
+  saveToIpfs (reader) {
+    let ipfsId;
+    const buffer = Buffer.from(reader.result);
+    this.ipfsApi.add(buffer, { progress: (prog) => console.log(`received: ${prog}`) })
+      .then((response) => {
+        console.log(response);
+        ipfsId = response[0].hash;
+        console.log(ipfsId);
+        this.setState({added_file_hash: ipfsId});
+      }).catch((err) => {
+        console.error(err);
+      })
+  }
+
+  /* jshint ignore:start */
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1>IPFS Registration</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, click on button <code>submit</code> to upload files.
         </p>
+        <Grid>
+          <Button 
+              bsStyle="primary" 
+              type="submit"> 
+              Send it 
+          </Button>
+        </Grid>
       </div>
     );
-  }
+  };
+  /* jshint ignore:end */
 }
 
 export default App;
